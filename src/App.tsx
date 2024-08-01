92c31f763f60a484fe7822b77be5724e23de3ba1
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-07-31 21:23:42 -0300

    Adicionado MUI ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { AppRoutes } from "./routes/AppRoutes";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
 
 export default function App() {
   return (
     <>
+      <CssBaseline />
       <AppRoutes />
     </>
   );
